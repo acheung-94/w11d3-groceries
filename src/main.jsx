f6315aa3eab5ec521c9ebc807b56c5461a346d7e
acheung-94 <acheung@ucdavis.edu>
2024-03-20 13:29:24 -0700

    complete setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import configureStore from './store/store';
+import { Provider } from 'react-redux';
+
+const store = configureStore()
+
+if (import.meta.env.MODE !== "production") {
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
 
